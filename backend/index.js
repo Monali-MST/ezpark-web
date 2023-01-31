b624a516280fed1d8287d90c5e46485fe08e4814
Monali Shiwanthika <shiwanthikamonali@gmail.com>
2023-01-31 23:30:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,17 @@
 import express from "express";
+import mysql from "mysql";
 
 const app= express()
+const db = mysql.createConnection({
+    host:"ezpark-db.cvhbqqtsx1je.ap-northeast-1.rds.amazonaws.com",
+    user:"admin",
+    password:"ezPark!123",
+    database:"EzPark"
+})
+
+app.get("/",(req,res)=>{
+    res.json("Hello this is the backend")
+})
 
 app.listen(8800, ()=>{
     console.log("Connected to backend!!")
