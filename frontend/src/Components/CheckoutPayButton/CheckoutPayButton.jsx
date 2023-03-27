c979639b30b47b3ff474297f95c3d2e198718c17
Monali Shiwanthika <shiwanthikamonali@gmail.com>
2023-03-27 21:46:36 +0530

    Update CheckoutPayButton.jsx

--- frontend/src/Components/CheckoutPayButton/CheckoutPayButton.jsx
+++ frontend/src/Components/CheckoutPayButton/CheckoutPayButton.jsx
@@ -1,42 +1,38 @@
-import React from 'react'
+import React from "react";
 import "./CheckoutPayButton.css";
 
-const CheckoutPayButton = () => {
-    const handlecheckbutton = () => {
-        fetch("http://localhost:8800/create-checkout-session", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            items: [
-              { id: 1, quantity: 1 }
-            ],
-          }),
-        })
-          .then((res) => {
-            if (res.ok) return res.json();
-            return res.json().then((json) => Promise.reject(json));
-          })
-          .then(({ url }) => {
-            window.location = url;
-          })
-          .catch((e) => {
-            console.error(e.error);
-          });
-      };
-    
-      return (
-        <div>
-          <button className='btn'
-            onClick={
-              handlecheckbutton
-            }
-          >
-            Pay Here
-          </button>
-        </div>
-      );
-}
+import Button from "react-bootstrap/Button";
 
-export default CheckoutPayButton
+const CheckoutPayButton = () => {
+  const handlecheckbutton = () => {
+    fetch("http://localhost:8800/create-checkout-session", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        items: [{ id: 1, quantity: 1 }],
+      }),
+    })
+      .then((res) => {
+        if (res.ok) return res.json();
+        return res.json().then((json) => Promise.reject(json));
+      })
+      .then(({ url }) => {
+        window.location = url;
+      })
+      .catch((e) => {
+        console.error(e.error);
+      });
+  };
+
+  return (
+    <div>
+      <Button variant="outline-primary" onClick={handlecheckbutton}>
+        Pay Now
+      </Button>
+    </div>
+  );
+};
+
+export default CheckoutPayButton;
