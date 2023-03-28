8834b189b68b799d997160b72d9ba44e77c2c202
Monali Shiwanthika <shiwanthikamonali@gmail.com>
2023-03-28 16:17:35 +0530

    Update AdminRefundRequest.jsx

--- frontend/src/Components/AdminRefundRequest/AdminRefundRequest.jsx
+++ frontend/src/Components/AdminRefundRequest/AdminRefundRequest.jsx
@@ -1,65 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Accordion from "react-bootstrap/Accordion";
 import Button from "react-bootstrap/Button";
 
 const AdminRefundRequest = () => {
+  const [requests, setRequests] = useState([]);
+
+  useEffect(() => {
+    const fetchAllRequests = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:8800/api/user/get/get_refund_request"
+        );
+        setRequests(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllRequests();
+  }, []);
+
   return (
-    <div>
-      <Accordion defaultActiveKey="0">
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>Refund Request #1</Accordion.Header>
-          <Accordion.Body>
-            <b>Reason :</b>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-            Booking ID:
-            <p>0154</p>
-            Paid Amount:
-            <p>Rs.250.00</p>
-            <Button variant="primary">Accept Refund</Button>{" "}
-            <Button variant="danger">Reject Reqest</Button>
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Refund Reques #2</Accordion.Header>
-          <Accordion.Body>
-            <b>Reason :</b>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-            Booking ID:
-            <p>0154</p>
-            Paid Amount:
-            <p>Rs.250.00</p>
-            <Button variant="primary">Accept Refund</Button>{" "}
-            <Button variant="danger">Reject Reqest</Button>
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="2">
-          <Accordion.Header>Refund Reques #3</Accordion.Header>
-          <Accordion.Body>
-            <b>Reason :</b>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-            Booking ID:
-            <p>0154</p>
-            Paid Amount:
-            <p>Rs.250.00</p>
-            <Button variant="primary">Accept Refund</Button>{" "}
-            <Button variant="danger">Reject Reqest</Button>
-          </Accordion.Body>
-        </Accordion.Item>
+    <div className="Refund-requests">
+      <Accordion defaultActiveKey={0}>
+        {requests.map((request, index) => (
+          <Accordion.Item eventKey={index}>
+            <Accordion.Header>
+              Refund Request #{request.Refund_Request_id}
+            </Accordion.Header>
+            <Accordion.Body>
+              <b>Reason :</b>
+              <p>{request.Reason}</p>
+              <b>Booking ID :</b>
+              <p>{request.Booking_id}</p>
+              <b>Paid Amount :</b>
+              <p>Rs.{request.PaymentAmount}.00</p>
+              <Button variant="primary">Accept Refund</Button>{" "}
+              <Button variant="danger">Reject Reqest</Button>
+            </Accordion.Body>
+          </Accordion.Item>
+        ))}
       </Accordion>
     </div>
   );
