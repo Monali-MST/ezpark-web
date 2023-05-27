03680e14140c32a47b4f0cdec38c8fad96b77a6e
udayanganiherath <udayanganiherath20@gmail.com>
2023-05-27 11:49:14 +0530

    Homepage added

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,13 +1,91 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <section>
-      <div>
-        
-      </div>
-    </section>
-  )
-}
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-3 col-lg-4 col-md-6">
+            <div>
+              <h3>Logo</h3>
+              <p class="mb-30 footer-desc">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
+                soluta facilis eos quia optio iusto odit atque eum tempore,
+                quisquam officiis vero veniam hic,
+              </p>
+            </div>
+          </div>
+          <div class="col-xl-2 offset-xl-1 col-lg-2 col-md-6">
+            <div class="">
+              <h4>Quick Link</h4>
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#" class="text-decoration-none">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="/signup" class="text-decoration-none">
+                    Register
+                  </a>
+                </li>
+                <li>
+                  <a href="/login" class="text-decoration-none">
+                    Login
+                  </a>
+                </li>
+                <li>
+                  <a href="/supoort" class="text-decoration-none">
+                    Support
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
 
-export default Footer
+          <div class="col-xl-3 col-lg-3 col-md-6">
+            <div>
+              <h4>Contact Us</h4>
+              <ul class="list-unstyled">
+                <li>
+                  <p>Katubedda,Moratuwa</p>
+                </li>
+                <li>
+                  <p>011-4545567</p>
+                </li>
+                <li>
+                  <p>EzPark@gmail.com</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-xl-3 col-lg-3 col-md-6">
+            <div>
+              <h4>Follow Us on</h4>
+              <div>
+          <ul class="d-flex gap-3 list-unstyled">
+            <li><a href="#"><i class="bi bi-facebook"></i></a></li>
+            <li><a href="#"><i class="bi bi-twitter"></i></a></li>
+            <li><a href="#"><i class="bi bi-instagram"></i></a></li>
+          </ul>
+        </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="d-flex justify-content-center">
+          <div class="copyright">
+            <p>
+              Developed and maintained by{" "}
+              <a href="#" target="_blank">
+                company
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- frontend/src/Pages/HomePage/HomePage.jsx
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Navbar from "../../Components/NavBar/NavBar";
 import Footer from "../../Components/Footer/Footer";
 import Button from "react-bootstrap/Button";
-
 import { useNavigate } from "react-router-dom";
 import "./HomePage.css";
 import myImage from "../../Assets/bgimg.jpg";
-import myImage1 from "../../Assets/vehicle_line.jpg"
+import myImage1 from "../../Assets/booking.jpg";
+import myImage2 from "../../Assets/OnlinePayment.jpg";
+import myImage3 from "../../Assets/user_Acc.png";
+import myImage4 from "../../Assets/parking.jpg";
+import myImage5 from "../../Assets/refund.jpg";
 import Logo from "../../Assets/logo_without_text.png";
+import Card from "react-bootstrap/Card";
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -25,58 +29,93 @@ const HomePage = () => {
       <div className="section">
         <Navbar />
 
-        <section id="coverpage">
-          <div
+        <div
+          style={{
+            backgroundImage: `url(${myImage})`,
+            backgroundSize: "cover",
+            height: "80vh",
+          }}
+        >
+          <div className="logo-section">
+            <div className="logo-img">
+              <img className="logo" src={Logo} alt="logo" />
+            </div>
+            <div className="logo-name">
+              <span className="yell">EZ </span>
+              <span className="blk">Park</span>
+            </div>
+          </div>
+          <p
             style={{
-              backgroundImage: `url(${myImage})`,
-              backgroundSize: "cover",
-              height: "80vh",
+              color: "#85857F",
+              fontWeight: "bold",
+              fontSize: "20px",
+              textAlign: "center",
             }}
           >
-            <div className="logo-section">
-              <div className="logo-img">
-                <img className="logo" src={Logo} alt="logo" />
-              </div>
-              <div className="logo-name">
-                <span className="yell">EZ </span>
-                <span className="blk">Park</span>
-              </div>
-            </div>
-            <p
-              style={{
-                color: "#85857F",
-                fontWeight: "bold",
-                fontSize: "20px",
-                textAlign: "center",
-              }}
-            >
-              PARKING AT YOUR FINGERTIPS
+            PARKING AT YOUR FINGERTIPS
+          </p>
+          <div
+            className="text"
+            style={{
+              color: "white",
+              textShadow: " 2px 2px 4px rgba(0, 0, 0, 0.5)",
+              fontSize: "3rem",
+              fontWeight: "bold",
+              marginLeft: "10vh",
+              paddingTop:"30vh",
+              marginLeft: "10vh",
+            }}
+          >
+            <p>
+              {" "}
+              Save Time.
+              <br />
+              Save Money.
             </p>
-
-            <div className="text" style={{}}>
-              <p style={{ color: "white", fontSize: "50px" }}>
-                {" "}
-                Save Time.
-                <br />
-                Save Money.
-              </p>
-            </div>
-            <Button className="start" onClick={handleClick}>
-              Get Started
-            </Button>
           </div>
+          <Button
+            variant="warning"
+            onClick={handleClick}
+            style={{
+              borderRadius: "5rem",
+              backgroundColor: "#F8D297",
+              alignItems: "center",
+             
+              marginLeft: "10vh",
+            }}
+          >
+            Get Started
+          </Button>{" "}
+        </div>
 
-          {/* <button className="start"onClick={handleClick}>VerEmail</button> */}
+        {/* <button className="start"onClick={handleClick}>VerEmail</button> */}
 
-          <h1 className="text1" style={{marginTop:"10px"}}>WE HAVE A SPOT FOR YOU. </h1>
-        </section>
-        <Footer />
+        <h1
+          className="text1"
+          style={{
+            marginTop: "10px",
+            color: "#FAA41E",
+            fontSize: "2rem",
+            textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+          }}
+        >
+          WE HAVE A SPOT FOR YOU.{" "}
+        </h1>
       </div>
       <div className="section">
         <div className="section-title">HOW IT WORKS</div>
         <div className="two-column-container">
           <div className="column">
-            <p style={{textAlign:"justify",margin:"50px",paddingTop:"100px",fontFamily:"bold",fontSize:"20px"}}>
+            <p
+              style={{
+                textAlign: "justify",
+                margin: "50px",
+                paddingTop: "100px",
+                fontFamily: "bold",
+                fontSize: "20px",
+              }}
+            >
               A vehicle parking system is an automated solution that simplifies
               the process of parking and managing vehicles in a parking
               facility. It includes features such as entry gates, sensors,
@@ -94,8 +133,7 @@ const HomePage = () => {
               <iframe
                 src="https://www.youtube.com/watch?v=CK7KYruoGZY"
                 title="Embedded video"
-
-                style={{width:"700px",height:"500px",marginTop:"75px"}}
+                style={{ width: "700px", height: "500px", marginTop: "75px" }}
               ></iframe>
             </div>
           </div>
@@ -105,22 +143,122 @@ const HomePage = () => {
         <div className="section-title">360 veiw</div>
       </div>
       <div className="section" style={{ backgroundColor: "black" }}>
-      <div
-            style={{
-              backgroundImage: `url(${myImage1})`,
-              backgroundSize: "cover",
-              height: "75vh",
-            }}
-          >
-      </div>
+        <div className="card-container">
+          <div>
+            <Card style={{ width: "16rem", height: "20rem" }}>
+              <Card.Img
+                variant="top"
+                src={myImage1}
+                style={{ height: "10rem" }}
+              />
+              <Card.Body>
+                <Card.Title>Online Bookings</Card.Title>
+                <Card.Text style={{ fontSize: "14px" }}>
+                  Allow users to book parking slots for their vehicles in
+                  advance. Users can select the date, time, and duration for
+                  which they need parking.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+          <div>
+            <Card
+              style={{ width: "16rem", height: "20rem", marginTop: "30vh" }}
+            >
+              <Card.Img
+                variant="top"
+                src={myImage2}
+                style={{ height: "10rem" }}
+              />
+              <Card.Body>
+                <Card.Title>Payment Processing</Card.Title>
+                <Card.Text style={{ fontSize: "14px" }}>
+                  Integrate a secure payment gateway to handle online payments
+                  for parking reservations. Users can make payments using debit
+                  cards or other available payment methods.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+          <div>
+            <Card style={{ width: "16rem", height: "20rem" }}>
+              <Card.Img
+                variant="top"
+                src={myImage3}
+                style={{ height: "10rem" }}
+              />
+              <Card.Body>
+                <Card.Title>User Accounts</Card.Title>
+                <Card.Text style={{ fontSize: "14px" }}>
+                  Provide user registration and login functionality, allowing
+                  users to create accounts to manage their parking bookings,
+                  view booking history, and update personal information.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+          <div>
+            <Card
+              style={{ width: "16rem", height: "20rem", marginTop: "30vh" }}
+            >
+              <Card.Img
+                variant="top"
+                src={myImage4}
+                style={{ height: "10rem" }}
+              />
+              <Card.Body>
+                <Card.Title>Availability Check</Card.Title>
+                <Card.Text style={{ fontSize: "14px" }}>
+                  Provide real-time availability of parking slots, allowing
+                  users to check if slots are available for their desired date
+                  and time.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+          <div>
+            <Card style={{ width: "16rem", height: "20rem" }}>
+              <Card.Img
+                variant="top"
+                src={myImage5}
+                style={{ height: "10rem" }}
+              />
+              <Card.Body>
+                <Card.Title>Cancellation and Refunds</Card.Title>
+                <Card.Text style={{ fontSize: "14px" }}>
+                  Allow users to cancel their parking reservations within a
+                  specified time frame and provide a refund based on the
+                  cancellation policy.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
       </div>
       <div
         className="section"
         style={{ backgroundColor: "#FAA41E", height: "75vh" }}
       >
-        <p> OUR SERVICES</p>
+        <p
+          style={{
+            fontSize: "50px",
+            color: "white",
+            paddingTop: "10vh",
+            paddingLeft: "5vh",
+          }}
+        >
+          BROWSERING ON YOUR PHONE?
+        </p>
+        <p style={{ fontSize: "50px", color: "white", paddingLeft: "5vh" }}>
+          Try out the Ez Park App on Android.
+        </p>
       </div>
-      <Footer />
+      <div
+        className="section"
+        style={{ backgroundColor: "#CBCBCB", height: "25vh" }}
+      >
+        <Footer />
+      </div>
     </div>
   );
 };
