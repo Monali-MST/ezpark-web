fef96c7ae09c03a226eb98d423b6ba4b792804c0
Nathali Fernando <nathalifernando70@gmail.com>
2023-06-14 10:32:20 +0530

    simple changes and formatting

--- frontend/src/Pages/StaticView/CurrentDate.js
+++ frontend/src/Pages/StaticView/CurrentDate.js
@@ -6,7 +6,7 @@ class CurrentDate extends Component {
     const year = today.getFullYear();
     const month = (today.getMonth() + 1).toString().padStart(2, '0');
     const day = today.getDate().toString().padStart(2, '0');
-    
+
     return `${day}-${month}-${year}`;
   }
 
